800a3f682fb93d86b9ff5a75758fd430665b8e96
Joey Liechty <joeyliechty@gmail.com>
2016-07-16 12:51:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ GATHERER_TYPEAHEAD_URI = 'http://gatherer.wizards.com/Handlers/InlineCardSearch.
 GATHERER_URI = 'http://gatherer.wizards.com/Handlers/Image.ashx?type=card'
 
 app = Chalice(app_name='magiccardbot')
-app.debug = True
 
 @app.route('/magic-cards', methods=['GET'])
 def magic_cards():
@@ -15,7 +14,7 @@ def magic_cards():
   card_name = urllib.quote_plus(json['text'])
   # try to derive the card name from a fragment
   cards_json = requests.get(GATHERER_TYPEAHEAD_URI % card_name).json()
-  if json['token'] == '5B4t9yJ044hLrDBYDKq8xK3A' and len(cards_json['Results']) > 0:
+  if json['token'] == '{{your token here}}' and len(cards_json['Results']) > 0:
     card_name = cards_json['Results'][0]['Name']
     # Get card image uri
     response = '{}&name={}'.format(GATHERER_URI, urllib.quote_plus(card_name))
